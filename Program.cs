36de87e1fd710401b742a151af040db6c062b89a
wasif farid <wsffarid1@gmail.com>
2021-09-13 11:51:20 +0530

    add contact

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
@@ -6,7 +7,50 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Address Book System");
+            ReadInput.PersonDetails();
+            Console.ReadLine();
+        }
+    }
+    class ReadInput
+    {
+        public static void PersonDetails()
+        {
+            Console.Write("Enter First Name: ");
+            String firstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+            var Addresses = Console.ReadLine();
+
+            Console.Write("Enter City : ");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter State : ");
+            string state = Console.ReadLine();
+
+            Console.Write("Enter ZipCode: ");
+            string zipCode = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter EmailId: ");
+            string Email = Console.ReadLine();
+            //Adding fetched details to list
+            List<string> PersonList = new List<string>();
+            PersonList.Add(firstName);
+            PersonList.Add(lastName);
+            PersonList.Add(city);
+            PersonList.Add(state);
+            PersonList.Add(zipCode);
+            PersonList.Add(Email);
+            foreach (string i in PersonList)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
